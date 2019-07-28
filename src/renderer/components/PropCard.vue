d8e6d4c072433cc1261a0f39e0360554c15793cf
Zrean Tofiq <zrean.tofiq@netlight.com>
2019-07-28 20:04:00 +0200

    Set up prop card

---
+++ src/renderer/components/PropCard.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  v-card(
+    v-if="selectedNode"
+    max-width="344"
+    class="mx-auto"
+    elevation=20
+    )
+    v-card-title.title
+      | {{selectedNode.data.name}}
+    v-card-text.body-1
+      | I'm card text
+  
+</template>
+
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch, Ref } from 'vue-property-decorator'
+
+import { vxm } from '../store'
+
+@Component
+export default class PropCard extends Vue {
+  get selectedNode() {
+    return vxm.graph.selectedNode
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.v-card {
+  position: absolute;
+}
+</style>
